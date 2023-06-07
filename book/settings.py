8afe2a88f41b528f485840c0d40c3ac231a84a74
Wayne <74553297+Lil-Vert@users.noreply.github.com>
2023-06-07 15:19:07 +0100

    Update settings.py

--- book/settings.py
+++ book/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'book.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR /'templates'],
+        'DIRS': [BASE_DIR /''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
