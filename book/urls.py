333397a2456de8692525da7ea6e62dbd5e2af29d
Dwayne <victormatthew626@gmail.comgit commit -m stating git config --global user.email victormatthew626@gmail.com>
2023-06-08 04:14:29 +0100

    resolved issue

--- book/urls.py
+++ book/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', include('bookApp.urls')),
 ]
 
-#if settings.DEBUG:
+if settings.DEBUG == False:
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
