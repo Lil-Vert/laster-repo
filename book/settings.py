4018c8502ade79036fe3e1987a628bb22b0851e6
Dwayne <victormatthew626@gmail.comgit commit -m stating git config --global user.email victormatthew626@gmail.com>
2023-06-11 04:53:45 +0100

    image set

--- book/settings.py
+++ book/settings.py
@@ -132,7 +132,7 @@ LOGOUT_REDIRECT_URL = ''
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 MEDIA_URL = "/media/"
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
