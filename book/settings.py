ad5073b104f275c3bc08e172330fbf284f706f20
Wayne <74553297+Lil-Vert@users.noreply.github.com>
2023-06-13 03:17:49 +0100

    Update settings.py

--- book/settings.py
+++ book/settings.py
@@ -134,13 +134,13 @@ LOGOUT_REDIRECT_URL = ''
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+MEDIA_URL = "/media/"
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ] 
-MEDIA_URL = "/media/"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
